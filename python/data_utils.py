c01818abada7e8459948f407f17bc4cc1932fe0e
Chen Yu <chenyu.nus@gmail.com>
2017-10-17 10:37:35 +0800

    Added hero2vec

--- python/data_utils.py
+++ python/data_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 
 with open('../crawl/match_id_nolimit') as file_:
     contents = []
@@ -6,12 +7,22 @@ with open('../crawl/match_id_nolimit') as file_:
         _content = _row.strip('\n').split(', ')[2]
         contents.append(_content)
 
+hero_info = json.load(open('../constants/hero_names.json'))
+hero_names = []
+hero_id_map = dict()
+for i, h in enumerate(hero_info.values()):
+    hero_names.append(h['localized_name'])
+    hero_id_map[str(h['id'])] = i
+
 
 def shuffle_list(_l):
     indices = np.random.permutation(len(contents))
     return [_l[i] for i in indices]
 
+
 shuffled_contents = shuffle_list(contents)
+
+
 # test_size = 1000
 # test_id = list(np.random.randint(len(content), size=test_size))
 # tr_id = [i for i in range(len(content)) if i not in test_id]
@@ -21,36 +32,52 @@ shuffled_contents = shuffle_list(contents)
 
 def parse(_c):
     [tw, tl] = _c.split('>')
-    hw = np.zeros((114,))
-    hl = np.zeros((114,))
-    hw[[int(h) for h in tw.split(':')]] = 1
-    hl[[int(h) for h in tl.split(':')]] = 1
+    hw = np.zeros((113,))
+    hl = np.zeros((113,))
+    hw[[hero_id_map[h] for h in tw.split(':')]] = 1
+    hl[[hero_id_map[h] for h in tl.split(':')]] = 1
     return hw, hl
 
 
-def hero_gen(_contents, batch_size):
-    i = 0
-    while True:
-        h = np.zeros((batch_size, 114))
-        for j in range(batch_size // 2):
-            _c = _contents[(i + j) % len(_contents)]
-            hw, hl = parse(_c)
-            h1[2 * j, :] = hw
-            h2[2 * j, :] = hl
-            win[2 * j, 0] = 1
-            h1[2 * j + 1, :] = hl
-            h2[2 * j + 1, :] = hw
-        i += 1
+def parse_1hot(_c):
+    [tw, tl] = _c.split('>')
+    hw = np.zeros((113, 5))
+    hl = np.zeros((113, 5))
+    for i, h in enumerate(tw.split(':')):
+        hw[hero_id_map[h], i] = 1
+    for i, h in enumerate(tl.split(':')):
+        hl[hero_id_map[h], i] = 1
+    return hw, hl
 
-        idx = np.random.permutation(batch_size)
-        yield h1[idx, :], h2[idx, :], win[idx]
+
+def get_heros_sampler(_contents, batch_size):
+    len_c = len(_contents)
+    _carray = np.array(_contents)
+    while True:
+        h = np.zeros((batch_size, 113, 5))
+        idx = np.random.choice(a=len_c, size=batch_size // 2)
+        for i, _match in enumerate(_carray[idx]):
+            h[i * 2, :, :], h[i * 2 + 1, :, :] = parse_1hot(_match)
+
+        yield h
+
+
+def get_cbow_pairs(hs):
+    cbow_xs = []
+    cbow_ys = []
+    for i in range(5):
+        yi = hs[:, :, i]
+        xi = np.sum(np.delete(hs, i, axis=2), axis=2)
+        cbow_xs.append(xi)
+        cbow_ys.append(yi)
+    return np.concatenate(cbow_xs, axis=0), np.concatenate(cbow_ys, axis=0)
 
 
 def draft_gen(_contents, batch_size):
     i = 0
     while True:
-        h1 = np.zeros((batch_size, 114))
-        h2 = np.zeros((batch_size, 114))
+        h1 = np.zeros((batch_size, 113))
+        h2 = np.zeros((batch_size, 113))
         win = np.zeros((batch_size, 1))
         for j in range(batch_size // 2):
             _c = _contents[(i + j) % len(_contents)]
@@ -68,8 +95,8 @@ def draft_gen(_contents, batch_size):
 
 def get_all_drafts(_contents):
     _size = _contents.shape[0]
-    h1 = np.zeros((_size, 114))
-    h2 = np.zeros((_size, 114))
+    h1 = np.zeros((_size, 113))
+    h2 = np.zeros((_size, 113))
     win = np.zeros((_size, 1))
     for j in range(_size // 2):
         _c = _contents[j]
--- python/hero2vec.py
+++ python/hero2vec.py
@@ -1,7 +1,52 @@
 import tensorflow as tf
 import data_utils as du
+import numpy as np
+import matplotlib.pyplot as plt
 
+EMBEDDING_DIM = 2
 batch_size = 100
-data_full = du.contents
-data_gen = du.get_hero_gen(data_full)
-# next(data_gen)
+n_heros = 113
+data_full = du.shuffled_contents
+data_gen = du.get_heros_sampler(data_full, batch_size)
+x = tf.placeholder(tf.float32, shape=(None, n_heros))
+y_label = tf.placeholder(tf.float32, shape=(None, n_heros))
+
+W1 = tf.Variable(tf.random_normal([n_heros, EMBEDDING_DIM]))
+b1 = tf.Variable(tf.random_normal([EMBEDDING_DIM]))
+hidden_representation = x @ W1 + b1
+W2 = tf.Variable(tf.random_normal([EMBEDDING_DIM, n_heros]))
+b2 = tf.Variable(tf.random_normal([n_heros]))
+prediction = tf.nn.softmax(hidden_representation @ W2 + b2)
+
+loss = tf.reduce_mean(-tf.reduce_sum(y_label * tf.log(prediction), 1))
+train_step = tf.train.RMSPropOptimizer(0.01).minimize(loss)
+
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+n_iters = 1000
+# train for n_iter iterations
+data_batch = next(data_gen)
+x_train, y_train = du.get_cbow_pairs(data_batch)
+input_dict = {x: x_train, y_label: y_train}
+for _ in range(n_iters):
+    sess.run(train_step, feed_dict=input_dict)
+    if _ % 30 == 0:
+        print('loss is : ', sess.run(loss,
+                                     feed_dict=input_dict))
+    data_batch = next(data_gen)
+    x_train, y_train = du.get_cbow_pairs(data_batch)
+    input_dict = {x: x_train, y_label: y_train}
+    if _ % 30 == 0:
+        print('next loss is : ', sess.run(loss,
+                                          feed_dict=input_dict))
+
+all_emb = sess.run(hidden_representation,
+                   feed_dict={x: np.eye(n_heros)})
+
+fig, ax = plt.subplots()
+ax.scatter(all_emb[:, 0], all_emb[:, 1])
+
+for i, txt in enumerate(du.hero_names):
+    ax.annotate(txt, (all_emb[i, 0], all_emb[i, 1]))
+plt.show()
